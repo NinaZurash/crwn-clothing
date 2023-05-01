58f29a461ad9d6955aa2fde728fc8a8012ebe18d
nzurashv <nino.zurashvili@cern.ch>
2023-05-01 09:58:14 +0400

    add sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
@@ -12,6 +13,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={logGoogleUser}>Sign in with Google Redirect</button>
     </div>
   )
 }
