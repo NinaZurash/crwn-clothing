f84f1d7a355e3871a29e2d01234fb93144d0effd
nzurashv <nino.zurashvili@cern.ch>
2023-06-14 13:37:41 +0400

    added cart functionallity

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import App from './App'
+import { UserProvider } from './components/contexts/user.context'
+import { ProductsProvider } from './components/contexts/products.context'
+import { CartProvider } from './components/contexts/cart.context'
+
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
